5c83eab33542e8ae4842068b263eec2e1ff1b186
donkey <iboggs@gmail.com>
2012-06-07 05:33:31 -0700

    More insulting

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 tail -f \#fuckshit/out |  
 while read foo ; do  
         if  echo $foo | grep "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} [0-9]\{2\}:[0-9]\{2\} <[^>]\+> iibot:"  ; then  
-            name=$(echo $foo | awk '{print $3}' | sed 's,<\(.*\)>,\1,')  
-            echo "/PRIVMSG #fuckshit :$name: WAT" > in ;
+            name=$(echo $foo | awk '{print $3}' | sed 's,<\(.*\)>,\1,') ;
+            message=$(echo $foo | cut -d ' ' -f5-) ;
+            echo "/PRIVMSG #fuckshit :$name: FUCK YOUR FUCKING '$message'" > in ;
         fi;  
 done
